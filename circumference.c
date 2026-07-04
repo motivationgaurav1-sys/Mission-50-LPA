a65eeef24d103320b5bade87ae56ad432b4599a7
Gaurav Pundir <motivation.gaurav1@gmail.com>
2026-07-04 12:43:03 +0000

    how to find circumference of a circle

---
+++ circumference.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    float circumference,r;
+    printf("value of radius: ");
+    scanf("%f",&r);
+    circumference=2*3.14*r;
+    printf("circumference of circle is %f",circumference);
+    return 0;
+
+}
