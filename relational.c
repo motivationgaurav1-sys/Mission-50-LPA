625852fd37a7fd6b66b7b0c602d66c7bde217f80
Gaurav Pundir <motivation.gaurav1@gmail.com>
2026-07-05 12:10:18 +0000

    how to find result of all realtional operators for  two numbers

---
+++ relational.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    int a,b;
+    printf("enter values: ");
+    scanf("%d%d",&a,&b);
+    printf("%d\n",a==b);
+    printf("%d\n",a!=b);
+    printf("%d\n",a>b);
+    printf("%d\n",a<b);
+    printf("%d\n",a>=b);
+    printf("%d\n",a<=b);
+    return 0;
+
+
+}
