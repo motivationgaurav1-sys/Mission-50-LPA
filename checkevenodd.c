ff89b18c11f999fe0d350f91b71085fcacd6ce30
Gaurav Pundir <motivation.gaurav1@gmail.com>
2026-07-06 10:46:04 +0000

    chek a number is even or odd

---
+++ checkevenodd.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int x;
+    printf("enter a number: ");
+    scanf("%d",&x);
+    if(x%2==0) {
+        printf("number is even.\n");
+
+    } else {
+        printf("number is odd.\n");
+    }
+return 0;
+
+}
