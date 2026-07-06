80051e547859b4d7784a0ac2b209062bbad15bed
Gaurav Pundir <motivation.gaurav1@gmail.com>
2026-07-06 11:24:17 +0000

    find smaller between two numbers

---
+++ samller1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int a,b;
+    printf("enter the numbers: ");
+    scanf("%d%d",&a,&b);
+    if(a<b) {
+        printf("%d is smaller.\n",a);
+
+    } else {
+        printf("%d is smaller.\n",b);
+
+    } 
+    return 0;
+}
