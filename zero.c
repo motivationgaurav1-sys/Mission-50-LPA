beda76eb83dd7fd7c1e01455d15db2a4c6d04ea0
Gaurav Pundir <motivation.gaurav1@gmail.com>
2026-07-06 11:43:42 +0000

    check number is zero or not

---
+++ zero.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int a;
+    printf("enter the numbers: ");
+    scanf("%d",&a);
+    if(a==0)  {
+        printf("zero");
+    } else {
+        printf("non zero");
+    } 
+    return 0;
+}
+    
