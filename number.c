e23feb69e84a4e072611f1bef9e90b1045a5ea0f
Gaurav Pundir <motivation.gaurav1@gmail.com>
2026-07-07 15:00:33 +0000

    check number is positive negative or zero

---
+++ number.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+int main(){
+    int number;
+    printf("enter a number");
+    scanf("%d",&number);
+    if(number>0) {
+        printf("positive number");
+
+    } else if (number<0) {
+        printf("negative number");
+    } else {
+        printf("zero");
+    }
+return 0;
+}
